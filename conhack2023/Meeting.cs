6941fd691dc6b96b04ee60e997d0985460a51749
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-10-22 14:14:14 -0400

    Updated professor, and meeting files. Also added commments to program

--- conhack2023/Meeting.cs
+++ conhack2023/Meeting.cs
@@ -1,10 +1,41 @@
 ﻿//nic
 using System;
+using conhack2023;
 
 namespace conhack2023
 {
     internal class Meeting
     {
+        public professor? host;
+        public List<student> participant = new List<student>();
+        public string? transcript;
+        public DateTime startTime;
+        public DateTime endTime;
+
+        public void scheduleMeeting()
+        {
+            Console.WriteLine("Input start date and time for the meeting:");
+            this.startTime = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Input end date and timer for the meeting:");
+            this.endTime = Convert.ToDateTime(Console.ReadLine());
+        }
+
+        public void initializeMeeting()
+        {
+
+        }
+
+        public void finishMeeting(lecture lecture)
+        {
+            string filename = Console.ReadLine();
+            saveToLecture(filename);
+        }
+
+        public void saveToLecture(string filename)
+        {
+
+        }
+
         string meetingToSQL()
         {
             return "";
--- conhack2023/Program.cs
+++ conhack2023/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Load stored professor, and student values
 
+// menu with options to navigate between courses and lectures
 
+// if in lecture recording, show comment functionality
